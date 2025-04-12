4b0f54aa6e02a8989f7fdbb03599333c7b691ccc
tanojrahul <tanojrahul19@gmail.com>
2025-04-12 16:05:40 +0530

    Backend reciver project setup

---
+++ Backend/ReciverBackend/FoodShareReciverModule/src/main/java/com/foodshare/backend/FoodShareReciverModuleApplication.java
@@ -0,0 +1,13 @@
+package com.foodshare.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodShareReciverModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodShareReciverModuleApplication.class, args);
+	}
+
+}
